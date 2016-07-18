1fc8aaa37f909baa46cefbb9da1a48fb5e2c570a
csxiaodiao <i@xiaodiao.me>
2016-07-18 17:25:54 +0800

    fix

--- website/db/migrate/20160718085325_add_omniauth_to_users.rb
+++ website/db/migrate/20160718085325_add_omniauth_to_users.rb
@@ -1,11 +1,27 @@
 class AddOmniauthToUsers < ActiveRecord::Migration[5.0]
   def up
+    add_column :users, :openid, :string, limit: 50, null: false
     add_column :users, :provider, :string
     add_column :users, :uid, :string
+    add_column :users, :sex, :integer
+    add_column :users, :name, :string, limit: 30
+    add_column :users, :country, :string, limit: 30
+    add_column :users, :province, :string, limit: 30
+    add_column :users, :headimgurl, :string, limit: 30
+    add_column :users, :subscribe_time, :datetime
+    add_column :users, :unsubscribe_time, :datetime
+
   end
 
   def down
+    remove_column :users, :openid
     remove_column :users, :provider
     remove_column :users, :uid
+    remove_column :users, :name
+    remove_column :users, :country
+    remove_column :users, :province
+    remove_column :users, :headimgurl
+    remove_column :users, :subscribe_time
+    remove_column :users, :unsubscribe_time
   end
 end
--- website/db/schema.rb
+++ website/db/schema.rb
@@ -65,20 +65,28 @@ ActiveRecord::Schema.define(version: 20160718085325) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                             default: "", null: false
+    t.string   "encrypted_password",                default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                     default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
+    t.string   "openid",                 limit: 50,              null: false
     t.string   "provider"
     t.string   "uid"
+    t.integer  "sex"
+    t.string   "name",                   limit: 30
+    t.string   "country",                limit: 30
+    t.string   "province",               limit: 30
+    t.string   "headimgurl",             limit: 30
+    t.datetime "subscribe_time"
+    t.datetime "unsubscribe_time"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
